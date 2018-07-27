77e14cc91cd8b1c9277622354b0675bce672dd2c
Bobby Brennan <bobby.brennan@gmail.com>
2018-07-27 01:01:31 +0000

    apps: better freedb service

--- apps/angular-template/src/app/services/freedb.service.ts
+++ apps/angular-template/src/app/services/freedb.service.ts
@@ -2,12 +2,38 @@ import {Injectable} from '@angular/core';
 const Client = require('../../../../../client');
 declare let require:any;
 
+const STORAGE_KEY = 'freedb_auth';
+
 @Injectable()
 export class FreeDBService {
   client:any;
+  user:any;
   constructor() {}
 
-  async initialize(host) {
-    this.client = new Client({host});
+  async initialize(options) {
+    this.client = new Client(options);
+  }
+
+  async maybeRestore() {
+    if (!window.localStorage) return;
+    let existing = window.localStorage.getItem(STORAGE_KEY);
+    if (!existing) return;
+    existing = JSON.parse(existing);
+    await this.initialize(existing);
+    this.user = await this.client.getUser();
+  }
+
+  async signIn(host) {
+    await this.initialize(host);
+    return new Promise((resolve, reject) => {
+      this.client.authorize(user => {
+        this.user = user;
+        if (window.localStorage) {
+          const toStore = {host, token: this.client.options.token};
+          window.localStorage.setItem(STORAGE_KEY, JSON.stringify(toStore))
+        }
+        resolve();
+      })
+    });
   }
 }
