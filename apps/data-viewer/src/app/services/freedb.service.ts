00d60e9b582089a6a79c13912f6b71a6080c40a3
Bobby Brennan <bobby.brennan@gmail.com>
2018-08-11 22:05:53 +0000

    update data-viewer from template

--- apps/data-viewer/src/app/services/freedb.service.ts
+++ apps/data-viewer/src/app/services/freedb.service.ts
@@ -1,13 +1,50 @@
-import {Injectable} from '@angular/core';
-const Client = require('../../../../../client');
+import {Injectable, NgZone} from '@angular/core';
+import {BehaviorSubject} from 'rxjs';
+
+declare let window:any;
 declare let require:any;
+const Client = require('../../../../../client');
+const settings = require('../../../../../.server-config.json');
+const CORE_HOST = settings.host;
+
+const STORAGE_KEY = 'freedb_auth';
 
 @Injectable()
 export class FreeDBService {
   client:any;
-  constructor() {}
+  user:any;
 
-  async initialize(host) {
-    this.client = new Client({host});
+  onUser = new BehaviorSubject(null);
+
+  constructor(private zone:NgZone) {
+    window.freedbService = this;
+    this.client = new Client({
+      hosts: {
+        core: {
+          location: CORE_HOST,
+        }
+      },
+      onUser: user => {
+        this.zone.run(_ => this.onUser.next(user));
+      }
+    });
+    this.maybeRestore();
+    this.onUser.subscribe(user => {
+      this.user = user;
+      if (!window.localStorage) return
+      const toStore = {
+        hosts: this.client.hosts,
+      };
+      window.localStorage.setItem(STORAGE_KEY, JSON.stringify(toStore))
+    })
+  }
+
+  async maybeRestore() {
+    if (!window.localStorage) return;
+    let existing:any = window.localStorage.getItem(STORAGE_KEY);
+    if (!existing) return;
+    existing = JSON.parse(existing);
+    if (!existing || !existing.hosts) return;
+    await this.client.setHosts(existing.hosts);
   }
 }
