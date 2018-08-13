454a150830526d9e22a698ef2d44799347857205
Bobby Brennan <bobby.brennan@gmail.com>
2018-08-13 23:02:04 +0000

    add server to todo app

---
+++ apps/todo/server.js
@@ -0,0 +1,12 @@
+let express = require('express');
+let app = express();
+
+const DIR = __dirname + '/dist/browser';
+const INDEX = require('fs').readFileSync(DIR + '/index.html');
+
+app.use(express.static(__dirname + '/dist/browser'));
+app.get('*', (req, res) => {
+  res.end(INDEX);
+})
+
+app.listen(4010);
