4cb41fb39f5d21c42da91c21f62ee3a99a743350
Bobby Brennan <bobby.brennan@gmail.com>
2018-09-05 20:00:32 +0000

    chat: show chats on homepage

--- apps/chat/src/app/home/home.component.ts
+++ apps/chat/src/app/home/home.component.ts
@@ -11,18 +11,41 @@ declare let require:any;
 })
 export class HomeComponent {
   @ViewChild('logInModal') logInModal;
+  listingChats:boolean;
   error:string;
   chatRoomName:string;
+
+  userChats:any[];
+  publicChats:any[];
+
   constructor(public freedb:FreeDBService, private router:Router) {
     this.freedb.onLogin.subscribe(user => {
-      console.log(user);
+      this.listChats();
     });
+    this.listChats();
+  }
+
+  async listChats() {
+    if (this.listingChats) return setTimeout(this.listChats.bind(this), 100);
+    this.listingChats = true;
+    this.publicChats = (await this.freedb.client.list('alpha_chat', 'conversation')).items;
+    let userMessages = await this.freedb.client.list('alpha_chat', 'message');
+    let chatIDs = [];
+    for (let message of userMessages.items) {
+      if (chatIDs.indexOf(message.conversationID) === -1) {
+        chatIDs.push(message.conversationID);
+      }
+    }
+    this.userChats = [];
+    for (let chatID of chatIDs) {
+      this.userChats.push(await this.freedb.client.get('alpha_chat', 'conversation', chatID));
+    }
+    this.listingChats = false;
   }
 
   async startChat() {
     this.error = null;
     let chatID = this.chatRoomName.replace(/\W+/g, '_');
-    console.log('creating', chatID);
     try {
       const chat = {title: this.chatRoomName || ''};
       chatID = await this.freedb.client.create('alpha_chat', 'conversation', chat, chatID);
