201d9ee024fcc82b1b0f27e9ce03815cfe89fccd
Bobby Brennan <bobby.brennan@gmail.com>
2018-10-02 17:02:00 +0000

    set default core proxy for server instances

--- cmd/lib/serve.js
+++ cmd/lib/serve.js
@@ -4,10 +4,16 @@ const npath = require('path');
 const YAML = require('yamljs');
 
 const CONFIG_FILE = npath.join(process.cwd(), 'OneDB.yml');
+const DEFAULT_CORE_HOST = "https://one-db.datafire.io";
 
 module.exports = function(opts) {
   if (fs.existsSync(CONFIG_FILE)) {
     opts = Object.assign({}, YAML.load(CONFIG_FILE), opts);
+  }
+  opts.namespaces = opts.namespaces || {};
+  opts.namespaces.proxy = opts.namespaces.proxy || {};
+  if (opts.namespaces.proxy.core === undefined) {
+    opts.namespaces.proxy.core = DEFAULT_CORE_HOST;
   }
   let server = new Server(opts);
   return server.listen(opts.port);
